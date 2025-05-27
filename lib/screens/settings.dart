43b5f905abc102b992670f81b03b608db0098cda
Marcin Malessa <marcin@malessa.pl>
2025-05-27 21:02:34 +0200

    Add more to settings

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -14,6 +15,39 @@ class _SettingsScreenState extends State<SettingsScreen> {
   final options = ['Opcja 1', 'Opcja 2', 'Opcja 3'];
   bool isChecked = false;
   bool _isPasswordVisible = false;
+  String segmentedButtonVelue = 'sec';
+  double sliderValue = 0.7;
+  String? _selectedOption;
+  final List<String> listOptions = [
+    'Opcja 1',
+    'Opcja 2',
+    'Opcja 3',
+    'Opcja 4',
+    'Opcja 5',
+    'Opcja 6',
+  ];
+
+  void _showPopup(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true, // Kliknięcie poza dialogiem go zamyka
+      barrierColor: Colors.black.withOpacity(0.5), // Overlay
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Okienko"),
+          content: Text("Akuku!"),
+          actions: [
+            TextButton(
+              child: Text("Zamknij"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,8 +58,8 @@ class _SettingsScreenState extends State<SettingsScreen> {
             title: const Text('Settings')
           ),
 
-          body: Padding(
-            padding: EdgeInsets.all(14),
+          body: SingleChildScrollView( // Scroll does not work. Use LayoutBuilder
+            padding: EdgeInsets.all(16),
             child: Column(
               children: [
                 const Row(
@@ -118,11 +152,88 @@ class _SettingsScreenState extends State<SettingsScreen> {
                     ),
                     Text('Coś jest lub czegoś nie ma')
                   ],
-                )
+                ),
+
+                const SizedBox(height: 15),
+
+                SegmentedButton(
+                  // style: SegmentedButton.styleFrom(
+                  //   backgroundColor: Colors.grey[200],
+                  //   foregroundColor: Colors.red,
+                  //   selectedForegroundColor: Colors.white,
+                  //   selectedBackgroundColor: Colors.green,
+                  // ),
+                  segments: const <ButtonSegment<String>>[
+                    ButtonSegment<String>(
+                      value: 'sec',
+                      label: Text('1 sec'),
+                      icon: Icon(Icons.lock_clock)
+                    ),
+                    ButtonSegment<String>(
+                        value: 'min',
+                        label: Text('1 min'),
+                        icon: Icon(Icons.punch_clock)
+                    ),
+                    ButtonSegment<String>(
+                        value: 'hour',
+                        label: Text('1 hour'),
+                        icon: Icon(Icons.lock_clock_sharp)
+                    ),
+                    ButtonSegment<String>(
+                        value: 'day',
+                        label: Text('1 day'),
+                        icon: Icon(Icons.calendar_view_day)
+                    ),
+                  ],
+                  selected: <String>{segmentedButtonVelue},
+                  onSelectionChanged: (Set<String> newSelection) {
+                    setState(() {
+                      segmentedButtonVelue = newSelection.first;
+                    });
+                  },
+                ),
+
+                const SizedBox(height: 15),
+
+                Slider(
+                    value: sliderValue,
+                    onChanged: (value) {
+                      setState(() {
+                        sliderValue = value;
+                      });
+                    }
+                ),
+
+                const SizedBox(height: 15),
+
+                ElevatedButton(
+                    onPressed: () => _showPopup(context),
+                    child: Text('Click me')
+                ),
+
+                const SizedBox(height: 15),
+
+                ListView(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  children: listOptions.map((option) {
+                    bool isSelected = option == _selectedOption;
+                    return ListTile(
+                      title: Text(option),
+                      tileColor: isSelected ? Colors.grey.withOpacity(0.2) : null,
+                      trailing: isSelected ? Icon(Icons.check, color: Colors.blue) : null,
+                      onTap: () {
+                        setState(() {
+                          _selectedOption = option;
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+
               ],
             )
           ),
-
           bottomNavigationBar: SafeArea(
             child: Padding(
               padding: EdgeInsets.all(16.0),
